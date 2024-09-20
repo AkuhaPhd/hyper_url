4b3deaed224fc86c6c309ec13b2261814547752e
Akuhaa <akuha@islacare.co.uk>
2024-09-20 12:22:10 +0100

    server

---
+++ hyperurl/src/service.rs
@@ -0,0 +1,31 @@
+use std::sync::RwLock;
+use std::collections::HashMap;
+use std::sync::{Arc};
+use std::str;
+use hyper::Request;
+use hyper::{Body, Response};
+use hyper::rt::{Future, Stream};
+
+use lazy_static::lazy_static;
+
+use crate::shortener::shorten_url;
+
+type UrlDb = Arc<RwLock<HashMap<String, String>>>;
+type Boxfut = Box<dyn Future<Item = Response<Body>, Error = hyper::Error> + Send>;
+
+lazy_static! {
+    static ref SHORT_URLS: UrlDb = Arc::new(RwLock::new(HashMap::new()));
+}
+
+pub(crate) fn url_service(req: Request<Body>) -> Boxfut {
+    let reply = req.into_body().concat2().map(move |chunk| {
+        let c = chunk.iter().cloned().collect::<Vec<u8>>();
+        let url_to_shorten = str::from_utf8(&c).unwrap();
+        let shorten_url = shorten_url(url_to_shorten);
+        SHORT_URLS.write().unwrap().insert(shorten_url, url_to_shorten.to_string());
+        let a = &*SHORT_URLS.read().unwrap();
+        Response::new(Body::from(format!("{:#?}", a)))
+    });
+
+    Box::new(reply)
+}
